5ff7f7dbc314fd82cbae370a9efa2bed066b70d4
Karim Ibrahim <40270714+chillman12@users.noreply.github.com>
2019-08-31 17:31:18 -0400

    Add project files.

---
+++ WebApp/Controllers/HelloController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Semester()
+        {
+            return View();
+        }
+
+        public ActionResult Testimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(String name) {
+
+            ViewData["name"] = name;
+
+            return View();
+
+        }
+    }
+}
